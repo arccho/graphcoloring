52c2e3d6b7221b8d9ee18baf7c1cfcf60c08a017
arccho <xzeno@orange.fr>
2018-11-13 14:36:47 +0100

    first comm

---
+++ GraphColoring.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 6), (4, 5), (5, 6)])
+plt.subplot(111)
+
+nx.draw(G, with_labels=True, font_weight='bold')
+
+G.nodes[1]['color'] = 'red'
+plt.show()
+#G.add_weighted_edges_from()
+a = G.nodes.data()
+print a.get(1)
